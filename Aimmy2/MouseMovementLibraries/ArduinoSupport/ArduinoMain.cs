20665f78ffff141299c967047d17ef323f423ba0
ts0ra <gaming.heroes354@gmail.com>
2024-05-24 22:19:36 +0700

    Add project files.

---
+++ Aimmy2/MouseMovementLibraries/ArduinoSupport/ArduinoMain.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MouseMovementLibraries.ArduinoSupport
+{
+    internal class ArduinoMain
+    {
+
+    }
+}
---
+++ Aimmy2/Other/GetSpecs.cs
@@ -0,0 +1,19 @@
+﻿using System.Management;
+
+namespace Aimmy2.Class
+{
+    internal class GetSpecs
+    {
+        // Reference: https://www.youtube.com/watch?v=rou471Evuzc
+        // Nori
+        public static string? GetSpecification(string HardwareClass, string Syntax)
+        {
+            ManagementObjectSearcher SpecsSearch = new("root\\CIMV2", "SELECT * FROM " + HardwareClass);
+            foreach (ManagementObject MJ in SpecsSearch.Get().Cast<ManagementObject>())
+            {
+                return Convert.ToString(MJ[Syntax])?.Trim();
+            }
+            return "Not Found";
+        }
+    }
+}
