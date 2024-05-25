62e94e5a6407cc4279b0b65b00a2a4594fb18a3d
ts0ra <gaming.heroes354@gmail.com>
2024-05-25 11:27:07 +0700

    add close method

--- Aimmy2/Class/Arduino.cs
+++ Aimmy2/Class/Arduino.cs
@@ -5,5 +5,14 @@ namespace Class
     internal class Arduino
     {
         public static HidDevice? arduino { get; set; }
+
+        public static void Close()
+        {
+            if (arduino != null)
+            {
+                arduino.CloseDevice();
+                arduino = null;
+            }
+        }
     }
 }
